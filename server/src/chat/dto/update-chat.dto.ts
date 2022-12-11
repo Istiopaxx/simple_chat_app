5b6506f235f7b608a1e8f166bcb3713b1fd4d3b6
happyjamy <juwhan1004@naver.com>
2022-12-12 00:05:33 +0900

    add chat dto

--- server/src/chat/dto/update-chat.dto.ts
+++ server/src/chat/dto/update-chat.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatDto } from './create-chat.dto';
+import { CreateChatRoomDto } from './create-chat-room.dto';
 
-export class UpdateChatDto extends PartialType(CreateChatDto) {}
+export class UpdateChatDto extends PartialType(CreateChatRoomDto) {}
